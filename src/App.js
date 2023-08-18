18a3fb2de2c0dc68c8bcd057901b06ac4f7d2087
Shivang Yadav <shivangyadav39@gmail.com>
2023-08-18 19:31:30 +0530

    a clean initial structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import Employee from './Components/Employee';
 
 function App() {
+  // console.log('we are about to list the employees');
+  // const showEmployees=true;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      {showEmployees ? 
+        <>
+        <Employee />
+        <Employee />
+        <Employee />
+        <Employee />
+        </>
+        :
+        <p>you cannot see the employees</p>
+      }
+        
+        {/* <img src={logo} className="App-logo" alt="logo" />
+        <h1>hello</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        </p> */}
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
+        </a> */}
+      
     </div>
   );
 }
